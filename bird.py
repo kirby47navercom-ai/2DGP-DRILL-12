1d1dd4945fd0db9b4f1c591c0fdcf1224dd2f412
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-04 17:16:06 +0900

    새 나는거 만듬

--- bird.py
+++ bird.py
@@ -18,16 +18,18 @@ FRAMES_PER_ACTION = 8
 class Bird:
     def __init__(self):
         self.image = load_image('bird_animation.png')
+        self.image_x = 918
+        self.image_y = 516
         self.x = 100
         self.y = random.randint(400, 600)
         self.frame=0
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 12
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
 
         pass
 
     def draw(self):
-        self.image.clip_composite_draw(int(self.frame) * 100, 100, 100, 100,0,'', 200, 100,200,100)
+        self.image.clip_composite_draw(int(self.frame)%5 * int(self.image_x/5),(2-int(self.frame) // 5) *int(self.image_y/3)-10, int(self.image_x/5)-5, int(self.image_y/3),0,'', 200, 100,100,100)
 
 
